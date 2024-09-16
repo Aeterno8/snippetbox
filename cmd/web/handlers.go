62b34fd91d2eb57b7947c1f66a408e06c31546c4
Aleksa Savic <savicaleksa.kg@gmail.com>
2024-09-16 13:19:09 +0200

    template cache, dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,105 +1,72 @@
 package main
 
 import (
-    "errors"
-    "fmt"
-    "html/template"
-    "net/http"
-    "strconv"
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
 
-    "github.com/Aeterno8/snippetbox/internal/models"
+	"github.com/Aeterno8/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
 
-    snippets, err := app.snippets.Latest()
-    if err != nil {
-        app.serverError(w, err)
-        return
-    }
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
 
-    for _, snippet := range snippets {
-        fmt.Fprintf(w, "%+v\n", snippet)
-    }
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
 
-    //files := []string{
-    //      "./ui/html/base.tmpl",
-    //      "./ui/html/partials/nav.tmpl",
-    //      "./ui/html/pages/home.tmpl",
-    //}
-
-    //ts, err := template.ParseFiles(files...)
-    //if err != nil {
-    //    app.serverError(w, err)
-    //    return
-    //}
-
-    //err = ts.ExecuteTemplate(w, "base", nil)
-    //if err != nil {
-    //    app.serverError(w, err)
-    //}
+	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
-        app.notFound(w)
-        return
-    }
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
 
-    snippet, err := app.snippets.Get(id)
-    if err != nil {
-         if errors.Is(err, models.ErrNoRecord) {
-            app.notFound(w)
-         } else {
-            app.serverError(w, err)
-         }
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
 
-         return
-    }
+		return
+	}
 
-    files := []string{
-        "./ui/html/base.tmpl",
-        "./ui/html/partials/nav.tmpl",
-        "./ui/html/pages/view.tmpl",
-    }
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
-    ts, err := template.ParseFiles(files...)
-    if err != nil {
-      app.serverError(w, err)
-      return
-    }
-
-    data := &templateData{
-        Snippet: snippet,
-    }
-
-    err = ts.ExecuteTemplate(w, "base", data)
-    if err != nil {
-      app.serverError(w, err)
-    }
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        app.clientError(w, http.StatusMethodNotAllowed)
-        return
-    }
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
 
-    title := "0 snail"
-    content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-    expires := 7
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
+	expires := 7
 
-    id, err := app.snippets.Insert(title, content, expires)
-    if err != nil {
-        app.serverError(w, err)
-        return
-    }
-    
-    http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,63 +1,70 @@
 package main
 
 import (
-    "database/sql"
-    "flag"
-    "log"
-    "net/http"
-    "os"
+	"database/sql"
+	"flag"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/Aeterno8/snippetbox/internal/models"
-    _ "github.com/go-sql-driver/mysql"
+	"github.com/Aeterno8/snippetbox/internal/models"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-    errorLog *log.Logger
-    infoLog  *log.Logger
-    snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
-    addr := flag.String("addr", ":4000", "Http network address")
+	addr := flag.String("addr", ":4000", "Http network address")
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 
-    dsn := flag.String("dsn", "web:pass@tcp(localhost:6033)/snippetbox?parseTime=true", "MySQL data source name")
+	flag.Parse()
 
-    flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
-    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
-    db, err := openDB(*dsn)
-    if err != nil {
-        errorLog.Fatal(err)
-    }
+	defer db.Close()
 
-    defer db.Close()
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
 
-    app := &application{
-        errorLog: errorLog,
-        infoLog: infoLog,
-        snippets: &models.SnippetModel{DB: db},
-    }
+	app := &application{
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
+	}
 
-    srv := &http.Server{
-        Addr:   *addr,
-        ErrorLog: errorLog,
-        Handler: app.routes(),
-    }
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-    infoLog.Printf("Starting server on %s", *addr)
-    err = srv.ListenAndServe()
-    errorLog.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
 
 func openDB(dsn string) (*sql.DB, error) {
-    db, err := sql.Open("mysql", dsn)
-    if err != nil {
-        return nil, err
-    }
-    if err = db.Ping(); err != nil {
-        return nil, err
-    }
-    return db, nil
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
